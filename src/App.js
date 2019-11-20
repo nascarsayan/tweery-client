cc19dffe8b08fc43015ea847d9cdf2f406bed794
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-11-20 15:00:36 +0530

    Added react spinner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { grommet } from "grommet/themes"
 import { Download, Schedule, Search } from 'grommet-icons'
 import { Grommet, TextInput, Box, Button, Text, DropButton } from 'grommet'
 import fileDownload from 'js-file-download'
+import { ClipLoader } from 'react-spinners'
 
 import DropContent from './components/DropContent'
 import IndMap from './components/IndMap'
@@ -15,13 +16,14 @@ class App extends Component {
     open: false,
     date: '',
     time: '12:00 am',
+    loading: false
   }
 
   onSearch = async (suggestions, term, date, time) => {
     const apiUri = process.env.REACT_APP_API_URI || 'http://localhost:3000/api'
-    console.log(apiUri)
+    this.setState({ loading: true })
     const response = await axios.get(`${apiUri}/query/recent?term=${term}${date ? `&since=${new Date(`${new Date(date).toLocaleDateString()} ${time}`).toISOString()}` : ''}`)
-    this.setState({ points: response.data, suggestions: [...suggestions, term] })
+    this.setState({ points: response.data, loading: false, suggestions: [...suggestions, term] })
   }
 
   dateToJSON = (date) => {
@@ -48,7 +50,8 @@ class App extends Component {
         suggestions,
         open,
         date,
-        time
+        time,
+        loading
       },
       onSearch,
       onDownload
@@ -59,62 +62,76 @@ class App extends Component {
     }
     return (
       <Grommet full theme={grommet}>
-        <Box align="center" justify="center" pad="medium" direction="row">
-          <Box align="center" pad={{ right: 'small' }}>
-            <Text textAlign="center">
-              Search For
-            </Text>
-          </Box>
-          <Box width="medium">
-            <TextInput
-              placeholder='Tweets'
-              value={term}
-              onChange={e => this.setState({ term: e.target.value })}
-              onKeyDown={e => {
-                if (e.key === 'Enter') {
-                  onSearch(suggestions, term, date, time)
-                }
-              }}
+        {(loading) ? (
+          <Box fill={true} align="center" justify="center">
+            <ClipLoader
+              sizeUnit={"px"}
+              size={150}
+              color={'#36D7B7'}
+              loading={loading}
             />
           </Box>
-          <Box align="center" pad={{ left: 'small' }}>
-            <Text textAlign="center">
-              since
-            </Text>
-          </Box>
-          <Box>
-            <DropButton
-              open={open}
-              onClose={() => this.setState({ open: false })}
-              onOpen={() => this.setState({ open: true })}
-              dropContent={
-                <DropContent date={date} time={time} onClose={onClose} />
-              }
-            >
-              <Box direction="row" gap="medium" align="center" pad="small">
-                <Text color={date ? undefined : "dark-5"}>
-                  {date
-                    ? `${new Date(date).toLocaleDateString()} ${time}`
-                    : "Select date & time"}
-                </Text>
-                <Schedule />
+        ) : (
+            <React.Fragment>
+              <Box align="center" justify="center" pad="medium" direction="row">
+                <Box align="center" pad={{ right: 'small' }}>
+                  <Text textAlign="center">
+                    Search For
+          </Text>
+                </Box>
+                <Box width="medium">
+                  <TextInput
+                    placeholder='Tweets'
+                    value={term}
+                    onChange={e => this.setState({ term: e.target.value })}
+                    onKeyDown={e => {
+                      if (e.key === 'Enter') {
+                        onSearch(suggestions, term, date, time)
+                      }
+                    }}
+                  />
+                </Box>
+                <Box align="center" pad={{ left: 'small' }}>
+                  <Text textAlign="center">
+                    since
+          </Text>
+                </Box>
+                <Box>
+                  <DropButton
+                    open={open}
+                    onClose={() => this.setState({ open: false })}
+                    onOpen={() => this.setState({ open: true })}
+                    dropContent={
+                      <DropContent date={date} time={time} onClose={onClose} />
+                    }
+                  >
+                    <Box direction="row" gap="medium" align="center" pad="small">
+                      <Text color={date ? undefined : "dark-5"}>
+                        {date
+                          ? `${new Date(date).toLocaleDateString()} ${time}`
+                          : "Select date & time"}
+                      </Text>
+                      <Schedule />
+                    </Box>
+                  </DropButton>
+                </Box>
+                <Button
+                  icon={<Search />}
+                  onClick={e => {
+                    onSearch(suggestions, term, date, time)
+                  }}
+                />
+                <Button
+                  icon={<Download />}
+                  onClick={_ => onDownload(points)}
+                />
               </Box>
-            </DropButton>
-          </Box>
-          <Button
-            icon={<Search />}
-            onClick={e => {
-              onSearch(suggestions, term, date, time)
-            }}
-          />
-          <Button
-            icon={<Download />}
-            onClick={_ => onDownload(points)}
-          />
-        </Box>
-        <Box>
-          <IndMap points={points} />
-        </Box>
+              <Box>
+                <IndMap points={points} />
+              </Box>
+            </React.Fragment>
+          )
+        }
       </Grommet>
     )
   }
